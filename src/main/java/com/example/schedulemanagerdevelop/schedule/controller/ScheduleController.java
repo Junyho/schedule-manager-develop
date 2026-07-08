9e88d7cbf0814062bfcb02af094db3ba97ee9c1b
junyho <inop159@naver.com>
2026-07-08 12:14:34 +0900

    로그인 인증 기능 추가

--- src/main/java/com/example/schedulemanagerdevelop/schedule/controller/ScheduleController.java
+++ src/main/java/com/example/schedulemanagerdevelop/schedule/controller/ScheduleController.java
@@ -1,5 +1,6 @@
 package com.example.schedulemanagerdevelop.schedule.controller;
 
+import com.example.schedulemanagerdevelop.common.exception.UnauthorizedException;
 import com.example.schedulemanagerdevelop.schedule.dto.CreateSchedule;
 import com.example.schedulemanagerdevelop.schedule.dto.ScheduleResponse;
 import com.example.schedulemanagerdevelop.schedule.dto.UpdateSchedule;
@@ -22,8 +23,9 @@ public class ScheduleController {
     @PostMapping
     public ResponseEntity<ScheduleResponse> create(
             @RequestBody CreateSchedule createSchedule,
-            @SessionAttribute(name = "loginUser")SessionUser sessionUser
+            @SessionAttribute(name = "loginUser", required = false)SessionUser sessionUser
     ){
+        validateLogin(sessionUser);
         return ResponseEntity.status(HttpStatus.CREATED).body(scheduleService.create(sessionUser.getId(), createSchedule));
     }
 
@@ -40,18 +42,26 @@ public class ScheduleController {
     @PatchMapping("/{id}")
     public ResponseEntity<ScheduleResponse> update(
             @PathVariable Long id,
-            @SessionAttribute(name = "loginUser")SessionUser sessionUser,
+            @SessionAttribute(name = "loginUser", required = false)SessionUser sessionUser,
             @RequestBody UpdateSchedule updateSchedule
     ) {
+        validateLogin(sessionUser);
         return ResponseEntity.ok(scheduleService.update(id, sessionUser.getId(), updateSchedule));
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(
             @PathVariable Long id,
-            @SessionAttribute(name = "loginUser")SessionUser sessionUser
+            @SessionAttribute(name = "loginUser", required = false)SessionUser sessionUser
     ) {
+        validateLogin(sessionUser);
         scheduleService.delete(id,sessionUser.getId());
         return ResponseEntity.noContent().build();
+    }
+
+    private void validateLogin(SessionUser sessionUser) {
+        if (sessionUser == null) {
+            throw new UnauthorizedException("로그인이 필요합니다.");
+        }
     }
 }
